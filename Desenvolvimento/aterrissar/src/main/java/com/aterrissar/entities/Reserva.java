decb36cb8c555f6da1cc77dcbe83e45696875f00
Andre-Franco1 <andrefranco3172@gmail.com>
2023-11-27 22:08:55 -0300

    ajuste no relacionamento entre passagem e reserva

--- Desenvolvimento/aterrissar/src/main/java/com/aterrissar/entities/Reserva.java
+++ Desenvolvimento/aterrissar/src/main/java/com/aterrissar/entities/Reserva.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Reserva {
@@ -24,7 +25,7 @@ public class Reserva {
 	@ManyToOne
 	private User user;
 	
-	@ManyToOne
+	@OneToMany(mappedBy = "reserva")
 	private List<Passagem> passagens;
 	
 	public Reserva() {}
