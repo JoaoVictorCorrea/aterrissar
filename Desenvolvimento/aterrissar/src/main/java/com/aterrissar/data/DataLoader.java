1a103dcc39f5cd48a196f069948006c98a3185f3
João Victor Correa <correa.joaov25@gmail.com>
2023-12-03 11:21:15 -0300

    Criação de User Padrão e Correção do atributo urlImg de Voo para aceitar mais caracteres

--- Desenvolvimento/aterrissar/src/main/java/com/aterrissar/data/DataLoader.java
+++ Desenvolvimento/aterrissar/src/main/java/com/aterrissar/data/DataLoader.java
@@ -1,18 +1,26 @@
 package com.aterrissar.data;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.aterrissar.entities.Aeroporto;
+import com.aterrissar.entities.User;
 import com.aterrissar.repositories.AeroportoRepository;
+import com.aterrissar.repositories.UserRepository;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 	
 	@Autowired
 	private AeroportoRepository aeroportoRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Override
     @Transactional
@@ -25,6 +33,20 @@ public class DataLoader implements CommandLineRunner {
 
             aeroportoRepository.save(aeroporto1);
             aeroportoRepository.save(aeroporto2);
+        }
+        
+        if(userRepository.count() == 0) {
+        	
+        	Date date = java.sql.Date.valueOf("1900-01-01");
+        	
+        	User user = new User("Admin", "0000000000", date , "1112345689", "admin@admin.com", "admin", true);
+        	
+        	//encriptografando senha
+    		String encryptedPassword = new BCryptPasswordEncoder().encode(user.getSenha());
+    		
+    		user.setSenha(encryptedPassword);
+        	
+        	userRepository.save(user);
         }
     }
 }
